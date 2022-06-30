a3611135f07c48a590ae235615093c6ac9bc79d2
AlexViti <96179738+AlexViti@users.noreply.github.com>
2022-06-30 15:21:57 +0200

    Add classes

---
+++ Document.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBiblioteca
+{
+    internal abstract class Document
+    {
+        public string Code { get; protected set; }
+        public string Title { get; set; }
+        public string Sector { get; set; }
+        public bool IsAvailable { get; set; }
+        public int Rack { get; set; }
+        public Author Author { get; set; }
+
+        protected Document(string code, string title, string sector, int rack, Author author)
+        {
+            Code = code;
+            Title = title;
+            Sector = sector;
+            IsAvailable = true;
+            Rack = rack;
+            Author = author;
+        }
+    }
+
+    internal class Book : Document
+    {
+        public long ISBN { get; set; }
+        public int Pages { get; set; }
+        
+        public Book(long isbn, string title, string sector, int rack, Author author, int pages)
+            : base(isbn.ToString(), title, sector, rack, author)
+        {
+            ISBN = isbn;
+            Pages = pages;
+        }
+    }
+
+    internal class DVD : Document
+    {
+        public long SerialNumber { get; set; }
+        public int Runtime { get; set; }
+
+        public DVD(long serialNumber, string title, string sector, int rack, Author author, int runtime)
+            : base(serialNumber.ToString(), title, sector, rack, author)
+        {
+            SerialNumber = serialNumber;
+            Runtime = runtime;
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBiblioteca
+{
+    internal abstract class Person
+    {
+        public string Surname { get; set; }
+        public string Name { get; set; }
+
+
+        protected Person(string surname, string name)
+        {
+            Surname = surname;
+            Name = name;
+        }
+    }
+    
+    internal class User : Person
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Phone { get; set; }
+        public List<Document> Documents { get; }
+
+        public User(string surname, string name, string email, string password, int phone) : base(surname, name)
+        {
+            Email = email;
+            Password = password;
+            Phone = phone;
+            Documents = new List<Document>();
+        }
+    }
+    
+    internal class Author : Person
+    {
+        public Author(string surname, string name) : base(surname, name)
+        {
+            
+        }
+    }
+}
